a5edc6a8c4e62062e58692bcc08fddd754e968bc
nxlak <161253470+nxlak@users.noreply.github.com>
2024-09-11 22:43:48 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,44 @@
-// место для вашего кода
+#pragma once
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "geo.h"
+#include "transport_catalogue.h"
+
+namespace transport_catalogue {
+namespace input {
+
+struct CommandDescription {
+    // Определяет, задана ли команда (поле command непустое)
+    explicit operator bool() const {
+        return !command.empty();
+    }
+
+    bool operator!() const {
+        return !operator bool();
+    }
+
+    std::string command;      // Название команды
+    std::string id;           // id маршрута или остановки
+    std::string description;  // Параметры команды
+};
+
+class InputReader {
+public:
+    /**
+     * Парсит строку в структуру CommandDescription и сохраняет результат в commands_
+     */
+    void ParseLine(std::string_view line);
+
+    /**
+     * Наполняет данными транспортный справочник, используя команды из commands_
+     */
+    void ApplyCommands(TransportCatalogue& catalogue);
+
+private:
+    std::vector<CommandDescription> commands_;
+};
+    
+} //namespace input
+} //namespace transport_catalogue 
